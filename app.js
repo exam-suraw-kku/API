b5bf4a4a63619648ed5176786ecb7cb7465da67a
GNF DEDSEC <61729539+gnfdedsec@users.noreply.github.com>
2020-08-24 12:55:16 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/*  วิธีแรกพิมพ์ออก Console log
+
 var express = require('express');
 var app = express();
 var fs = require("fs");
@@ -9,4 +11,22 @@ app.get('/students', (req, res) => {
 });
 });
 app.listen(8080);
-console.log('Server running at http://localhost:8080/students');
+console.log('Server running at http://localhost:8080/students');*/
+// วิธีที่ 2 ใช้ Post man เทสได้
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+app.get('/students', function (req, res) { //เซต get methord
+   fs.readFile( __dirname + "/" + "students.json", 'utf8', function (err, data) {
+      console.log( data );
+      res.end( data );
+   });
+})
+
+var server = app.listen(8080, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
+
