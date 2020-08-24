11cf9103b825baa3ce11c0cd6a3959d7e5731a11
GNF DEDSEC <61729539+gnfdedsec@users.noreply.github.com>
2020-08-24 11:51:46 +0700

    Update app.js
    last fix bug

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
 var fs = require("fs");
 app.get('/students', (req, res) => {
-  fs.readFile(__dirname + "/" + "studnts.json", 'utf8', function(err, data)
-  consloe.log(data);
-  res.end(data);
+  fs.readFile('students.json', (err, data) => {
+    if (err) throw err;
+    let student = JSON.parse(data);
+    console.log(student);
 });
-
-app.listen(8080, () => {
- var host =server.address().address
- var port =server.address().port
-  console.log('Start server at port http://%s:%s",host,port)
- 
-  }
 });
+app.listen(8080);
+console.log('Server running at http://localhost:8080/students');
